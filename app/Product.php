37069a96870b2edae415bfe7cdb3031354bf004a
Guilherme Fontans <guilherme.fontans@gmail.com>
2021-07-06 23:15:42 -0300

    Adiçao de factories para store, product e category

--- app/Product.php
+++ app/Product.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    protected $fillable = ['name', 'description', 'body', 'slug'];
+
     public function store()
     {
         return $this->belongsTo(Store::class);
